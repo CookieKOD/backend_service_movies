e692dadc0121525e32c0932ae9d0aaddcc72498d
CookieKOD <107273814+CookieKOD@users.noreply.github.com>
2024-04-27 21:05:16 +0000

    Add files via upload

---
+++ models/movies.models.js
@@ -0,0 +1,22 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        title: { type: String, required: true },
+        director: { type: String, required: true },
+        synopsis: { type: String, required: true },
+        releaseDate: { type: Date, required: true },
+      },
+      { timestamps: true }
+    );
+  
+    schema.method("toJSON", function () {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+  
+    const Movie = mongoose.model("movie", schema);
+    return Movie;
+  };
+
+  
